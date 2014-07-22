7e9170c652ccbb2bfc20051f8d9df8400218af02
nilton <nilton@note>
2014-07-22 08:42:15 -0300

    Sistema - bean categoria com funcao salvar no banco criada

--- src/main/java/com/hypervia/livraria/view/CategoriaBean.java
+++ src/main/java/com/hypervia/livraria/view/CategoriaBean.java
@@ -4,8 +4,11 @@
  */
 package com.hypervia.livraria.view;
 
+import com.hypervia.livraria.controller.CategoriaJpaController;
+import com.hypervia.livraria.model.Categoria;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
 
 /**
  *
@@ -15,9 +18,24 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 public class CategoriaBean {
 
-    /**
-     * Creates a new instance of CategoriaBean
-     */
+    private Categoria cat = new Categoria();
+    private CategoriaJpaController dao;
+    private EntityManager em;
     public CategoriaBean() {
     }
+    public void gravar(){
+        dao = new CategoriaJpaController(null);
+        dao.create(cat);
+        this.cat = new Categoria();
+    }
+
+    public Categoria getCat() {
+        return cat;
+    }
+
+    public void setCat(Categoria cat) {
+        this.cat = cat;
+    }
+    
+    
 }
